ed448ec41d0def7a4368114609dd52b6cde416c1
MikeMcCready <pixelpres@gmail.com>
2021-02-12 16:10:59 -0700

    For Scene 2: Update interaction to only allow the index finger to trigger the buttons, prevent the buttons from being pressed more than one, track if the 1st and 2nd buttons were selected, if so, show the Go Inside button after 5 seconds. The Go Inside button currently goes to 2E3A, but can easily be changed. If this works, we'll apply similar options for scenes 3 and 5.

---
+++ Assets/Scripts/SceneTracker.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneTracker : MonoBehaviour
+{
+    public int sceneCount = 0;
+    public bool button1Clicked = false;
+    public bool button2Clicked = false;
+    public bool button3Clicked = false;
+    private GameObject option2Button;
+    private GameObject option3Button;
+    private Text option1ButtonText;
+    private choiceSelection choiceScript;
+    void Start()
+    {
+        // makes this object not get destroyed between scenes
+        // need this so we can track progress between scenes
+        DontDestroyOnLoad(this.gameObject); 
+    }
+    public void InvokeSuccess()
+    {
+        Invoke("Success", 5f);
+    }
+    private void Success()
+    {
+        // create references to the buttons and script
+        option2Button = GameObject.Find("Option #2");
+        option3Button = GameObject.Find("Option #3");
+        option1ButtonText = GameObject.Find("Option #1").GetComponentInChildren<Text>();
+        choiceScript = GameObject.Find("Option #1").GetComponent<choiceSelection>();
+
+        // disable 2nd and 3rd buttons
+        option2Button.SetActive(false);
+        option3Button.SetActive(false);
+        // update the text for the first button and change what scene it goes to
+        option1ButtonText.text = "Go Inside";
+        choiceScript.nextScene = "2E3A";
+
+        // reset the values
+        button1Clicked = false;
+        button1Clicked = false;
+        button3Clicked = false;
+        sceneCount = 0;
+    }
+}
--- Assets/Scripts/choiceSelection.cs
+++ Assets/Scripts/choiceSelection.cs
@@ -11,8 +11,17 @@ public class choiceSelection : MonoBehaviour
     //private SteamVR_Input_Sources inputSource;
     public string nextScene;
     public GameObject mainCanvas;
-    //bool buttonDown = false;
-
+    private SceneTracker sceneTracker;
+    private void Start()
+    {
+        // create a reference to the Scene Tracker which is used to track progress
+        sceneTracker = GameObject.Find("SceneTracker").GetComponent<SceneTracker>();
+        // if the first two buttons have been clicked
+        if (sceneTracker.sceneCount >= 2)
+        {
+            sceneTracker.InvokeSuccess();
+        }
+    }
 
     private void Update()
     {
@@ -23,14 +32,33 @@ public class choiceSelection : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        //if (other.gameObject.tag == "hand")
+        // only if the index finger press the button
+        if (other.gameObject.name.Contains("finger_index"))
         {
-            //*play matching scene or clip*
-            SceneManager.LoadScene(nextScene);
-            Debug.Log("Button Selected");
-            mainCanvas.SetActive(false);
-            mainCanvas.SetActive(true);
+            // if the button clicked is the first button and it hasn't been clicked before
+            if (this.gameObject.name == "Option #1" && !sceneTracker.button1Clicked)
+            {
+                // increase sceneCount by 1 and indicate the button has been clicked
+                sceneTracker.sceneCount++;
+                sceneTracker.button1Clicked = true;
+                LoadNewScene();
+            }
+            // if the button clicked is the second button and it hasn't been clicked before
+            else if (this.gameObject.name == "Option #2" && !sceneTracker.button2Clicked)
+            {
+                // increase sceneCount by 1 and indicate the button has been clicked
+                sceneTracker.sceneCount++;
+                sceneTracker.button2Clicked = true;
+                LoadNewScene();
+            }
         }
-
+    }
+    private void LoadNewScene()
+    {
+        //*play matching scene or clip*
+        SceneManager.LoadScene(nextScene);
+        //Debug.Log("Button Selected");
+        mainCanvas.SetActive(false);
+        mainCanvas.SetActive(true);
     }
 }
